22d31d615f189cbd688517538a1cf223fdba1cb0
WillianM19 <willianm1928@gmail.com>
2024-12-03 17:46:02 -0300

    feat(userCard): versão desktop do componente

--- src/components/atoms/UserCard/index.tsx
+++ src/components/atoms/UserCard/index.tsx
@@ -7,10 +7,17 @@ interface UserCardProps {
 
 export default function UserCard({userName, userImage}: UserCardProps) {
   return (
-	<div className="h-[150px] rounded-2xl bg-gradient-to-r from-red-500 to-red-900 flex items-center p-5 gap-3 relative">
-    <Image src={userImage} alt="" width={70} height={70} className="bg-red-950 w-[70px] h-[70px] rounded-full"  />
+	<div className="h-[150px] rounded-2xl bg-gradient-to-r from-red-500 to-red-900 flex items-center p-5 gap-3 relative md:h-[278px] md:flex-col md:justify-center">
+    <div className="relative w-20 h-20 md:w-32 md:h-32 bg-red-950 rounded-full overflow-hidden">
+      <Image src={userImage} alt="" layout="fill"  />
+    </div>
     <p className="text-[24px] text-white">{userName}</p>
-    <Image src="images/bg_card-img.svg" alt="" width={378} height={230} className="absolute bottom-0 right-0"  />
+    <div className="absolute bottom-0 right-0 md:-left-36 w-[378px] h-[180px] md:w-[655px] md:h-[325px]">
+      <Image src="images/bg_card-img.svg" alt="" layout="fill"   />
+    </div>
+    <div className="hidden absolute bottom-0 right-36 w-[378px] h-[230px] md:w-[655px] md:h-[270px] md:right-16 md:block">
+      <Image src="images/bg_card-img2.svg" alt="" layout="fill"   />
+    </div>
 	</div>
   );
 }
