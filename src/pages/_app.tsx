984bc9dbf3d748ab12cc5c191e6d08e36404257e
WillianM19 <willianm1928@gmail.com>
2025-02-04 17:11:41 -0300

    feat: redirect para tela de login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,21 @@ import BackToTop from "@/components/atoms/BackToTop";
 import Footer from "@/components/molecules/Footer";
 import Header from "@/components/molecules/Header";
 import "@/styles/globals.css";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps: { session, ...pageProps }}: AppProps) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session && session.status == "unauthenticated") {
+      router.replace("/")
+    }
+  }, [session])
+
   return (
     <SessionProvider session={session}>
       <div className="flex flex-col min-h-[100vh]">
