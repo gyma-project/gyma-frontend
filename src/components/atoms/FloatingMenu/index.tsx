12c95f9ffceb7a732adbffd93fe5bffa7a160b57
WillianM19 <willianm1928@gmail.com>
2025-02-12 17:11:56 -0300

    feat: adiciona botões de redirecionamento

--- src/components/atoms/FloatingMenu/index.tsx
+++ src/components/atoms/FloatingMenu/index.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function FloatingMenu() {
+
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
       {!isOpen && (
         <div
-          className="fixed left-[-8px] hover:left-0 transition-all top-[50%] bg-red-100 w-[40px] h-[100px] rounded-r-3xl flex items-center z-40 cursor-pointer max-md:hidden  active:shadow-md"
+          className="fixed left-[-8px] hover:left-0 transition-all top-[50%] translate-y-[-50%] bg-red-100 w-[40px] h-[100px] rounded-r-3xl flex items-center z-40 cursor-pointer max-md:hidden  active:shadow-md"
           onClick={() => setIsOpen(!isOpen)}
         >
           <Image
@@ -23,9 +26,18 @@ export default function FloatingMenu() {
       <div
         className={`fixed ${
           isOpen ? "left-[8px]" : "left-[-100px]"
-        } top-[50%] translate-y-[-50%] z-50 bg-red-100 w-[90px] h-[322px] rounded-2xl transition-all shadow-lg`}
+        } top-[50%] translate-y-[-50%] z-50 bg-red-100 w-[90px] h-[300px] rounded-2xl transition-all shadow-lg flex flex-col justify-evenly`}
       >
-        <h1></h1>
+        <div onClick={() => router.push('/user/create')} className="w-full flex justify-center cursor-pointer hover:scale-105 transition-all">
+            <Image src="icons/icon-register-user.svg" alt="" width={60} height={60}/>
+        </div>
+        <div onClick={() => router.push('/transactions/create')} className="w-full flex justify-center cursor-pointer hover:scale-105 transition-all">
+            <Image src="icons/icon-register-transaction.svg" alt="" width={60} height={60}/>
+        </div>
+        <div onClick={() => router.push('/training/scheduler')} className="w-full flex justify-center cursor-pointer hover:scale-105 transition-all">
+            <Image src="icons/icon-scheduler-training.svg" alt="" width={60} height={60}/>
+        </div>
+        
       </div>
       {isOpen && (
         <div
