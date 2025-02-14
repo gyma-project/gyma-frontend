b2ca12a18c78ecedb6b402e0f902434c3f6269cd
WillianM19 <willianm1928@gmail.com>
2025-02-14 17:24:28 -0300

    feat: adiciona children ao botão

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -10,7 +10,7 @@ export default function Button({children, otherButtonProps}: buttonProps) {
           <button
             className="bg-red-500 text-white px-4 py-2 rounded-full w-full md:w-72 hover:scale-[1.02] hover:shadow-lg active:scale-110 transition-all" {...otherButtonProps}
           >
-            Fazer login
+            {children}
           </button>
     )
 }
