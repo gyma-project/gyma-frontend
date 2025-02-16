f5d619630326dd355ca962078aeab25679f89e31
WillianM19 <willianm1928@gmail.com>
2025-02-16 11:31:07 -0300

    fix: corrigindo callback para retoranar acesstoken

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,6 +1,5 @@
-// axiosInstance.ts
 import axios from "axios";
-import Cookies from "js-cookie";
+import { getSession } from "next-auth/react"; // Importa getSession
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8081",
@@ -10,14 +9,22 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get("next-auth.session-token");
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+  async (config) => {
+    // Obtém a sessão do NextAuth
+    const session = await getSession();
+    console.log("Sessão do NextAuth:", session); // Verifica o conteúdo da sessão
+
+    if (session && session.accessToken) {
+      // Se o token de acesso estiver presente, o adiciona ao header
+      config.headers["Authorization"] = `Bearer ${session.accessToken}`;
+    } else {
+      console.log("Nenhum token encontrado na sessão.");
     }
+
     return config;
   },
   (error) => {
+    console.error("Erro no interceptor:", error);
     return Promise.reject(error);
   }
 );
