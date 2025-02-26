7403c610995a74bcc848c356efbb71c898700767
keilanyg <keilanydes@gmailcom>
2025-02-25 22:22:52 -0300

    fix: correcao do bug (GET) de listagem de usuarios

--- src/service/api/users.ts
+++ src/service/api/users.ts
@@ -1,4 +1,3 @@
-import axiosInstance from "@/service/axios";
 
 interface Role {
     id: number;
@@ -36,34 +35,3 @@ interface ApiResponse {
     last: boolean;
     empty: boolean;
 }
-
-// Função para buscar perfis com tipagem correta da resposta.
-export const getProfiles = async (page: number = 0, size: number = 10): Promise<ApiResponse> => {
-    try {
-        // Informando ao Axios que a resposta será do tipo ApiResponse.
-        const response = await axiosInstance.get<ApiResponse>('/profiles', {
-            params: { page, size }
-        });
-
-        if (response.status !== 200) {
-            throw new Error("Erro ao buscar perfis");
-        }
-
-        return response.data;  // Aqui a resposta é automaticamente tipada como ApiResponse.
-    } catch (error) {
-        console.error("Erro:", error);
-        return {
-            totalElements: 0,
-            totalPages: 0,
-            size: 0,
-            content: [],
-            number: 0,
-            sort: [],
-            numberOfElements: 0,
-            pageable: {},
-            first: true,
-            last: true,
-            empty: true,
-        };
-    }
-};
