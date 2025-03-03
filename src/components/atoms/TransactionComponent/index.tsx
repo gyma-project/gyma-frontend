60a32cd561303c4eb5aee8e15853e77dda046665
WillianM19 <willianm1928@gmail.com>
2025-03-03 13:47:48 -0300

    feat(transaction): adiciona data as transações

--- src/components/atoms/TransactionComponent/index.tsx
+++ src/components/atoms/TransactionComponent/index.tsx
@@ -5,7 +5,7 @@ export const categoriesConversion = {
   SALARIES: "Salário",
   EQUIPAMENT: "Equipamento",
   OTHERS: "Outros",
-}
+};
 
 interface TransactionComponentProps {
   value: number;
@@ -14,30 +14,58 @@ interface TransactionComponentProps {
   createdAt: string;
 }
 
-
 export default function TransactionComponent({
   value,
   description,
   category,
-  createdAt
+  createdAt,
 }: TransactionComponentProps) {
-
   const isDebit = value < 0;
 
   return (
     <div className="h-[119px] py-[30px] px-[20px] bg-red-50 hover:bg-red-100 transition-all  rounded-xl shadow-md flex justify-between items-center">
       <div className="flex gap-4">
-        <div className={`h-[60px] w-[60px] flex items-center justify-center ${isDebit ? 'bg-red-500' : 'bg-lime-500'}  rounded-xl`}>
-          <Image src={`/icons/icon-transaction-${isDebit ? "debit" : "increment"}.svg`} alt="money" width={30} height={30} />
+        <div
+          className={`h-[60px] w-[60px] flex items-center justify-center ${
+            isDebit ? "bg-red-500" : "bg-lime-500"
+          }  rounded-xl`}
+        >
+          <Image
+            src={`/icons/icon-transaction-${
+              isDebit ? "debit" : "increment"
+            }.svg`}
+            alt="money"
+            width={30}
+            height={30}
+          />
         </div>
         <div>
-          <p className={`text-[22px] font-bold ${isDebit ? 'text-red-500' : 'text-lime-500'}`}>R$ {isDebit ? value * -1 : value}</p>
-          <p className={isDebit ? 'text-red-500' : 'text-lime-500'}><strong>{isDebit ? 'Despesa: ' : 'Receita: '}</strong>{description}</p>
+          <p
+            className={`text-[22px] font-bold ${
+              isDebit ? "text-red-500" : "text-lime-500"
+            }`}
+          >
+            R$ {isDebit ? value * -1 : value}
+          </p>
+          <p className={isDebit ? "text-red-500" : "text-lime-500"}>
+            <strong>{isDebit ? "Despesa: " : "Receita: "}</strong>
+            {description}
+          </p>
         </div>
       </div>
       <div>
-       <p>Tipo: {categoriesConversion[category]}</p>
-       <p>Data:</p>
+        <p
+          className={`text-right ${isDebit ? "text-red-500" : "text-lime-500"}`}
+        >
+          <strong>Tipo: </strong>
+          {categoriesConversion[category]}
+        </p>
+        <p
+          className={`text-right ${isDebit ? "text-red-500" : "text-lime-500"}`}
+        >
+          <strong>Data: </strong>
+          {createdAt}
+        </p>
       </div>
     </div>
   );
