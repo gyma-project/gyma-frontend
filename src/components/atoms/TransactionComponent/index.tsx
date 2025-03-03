ebf671d81cd4d5ea350e2f107b5da5b044be8ddb
WillianM19 <willianm1928@gmail.com>
2025-03-03 13:45:10 -0300

    feat(trasactions): adiciona tipo de transação

--- src/components/atoms/TransactionComponent/index.tsx
+++ src/components/atoms/TransactionComponent/index.tsx
@@ -1,25 +1,43 @@
 import Image from "next/image";
 
+export const categoriesConversion = {
+  MEMBERSHIP: "Mensalidade",
+  SALARIES: "Salário",
+  EQUIPAMENT: "Equipamento",
+  OTHERS: "Outros",
+}
+
 interface TransactionComponentProps {
   value: number;
   description: string;
+  category: keyof typeof categoriesConversion;
+  createdAt: string;
 }
+
 
 export default function TransactionComponent({
   value,
   description,
+  category,
+  createdAt
 }: TransactionComponentProps) {
 
   const isDebit = value < 0;
 
   return (
-    <div className="h-[119px] py-[30px] px-[20px] bg-red-50 hover:bg-red-100 transition-all flex gap-4 rounded-xl shadow-md">
-      <div className={`h-[60px] w-[60px] flex items-center justify-center ${isDebit ? 'bg-red-500' : 'bg-lime-500'}  rounded-xl`}>
-        <Image src={`/icons/icon-transaction-${isDebit ? "debit" : "increment"}.svg`} alt="money" width={30} height={30} />
+    <div className="h-[119px] py-[30px] px-[20px] bg-red-50 hover:bg-red-100 transition-all  rounded-xl shadow-md flex justify-between items-center">
+      <div className="flex gap-4">
+        <div className={`h-[60px] w-[60px] flex items-center justify-center ${isDebit ? 'bg-red-500' : 'bg-lime-500'}  rounded-xl`}>
+          <Image src={`/icons/icon-transaction-${isDebit ? "debit" : "increment"}.svg`} alt="money" width={30} height={30} />
+        </div>
+        <div>
+          <p className={`text-[22px] font-bold ${isDebit ? 'text-red-500' : 'text-lime-500'}`}>R$ {isDebit ? value * -1 : value}</p>
+          <p className={isDebit ? 'text-red-500' : 'text-lime-500'}><strong>{isDebit ? 'Despesa: ' : 'Receita: '}</strong>{description}</p>
+        </div>
       </div>
       <div>
-        <p className={`text-[22px] font-bold ${isDebit ? 'text-red-500' : 'text-lime-500'}`}>R$ {isDebit ? value * -1 : value}</p>
-        <p className={isDebit ? 'text-red-500' : 'text-lime-500'}><strong>{isDebit ? 'Despesa: ' : 'Receita: '}</strong>{description}</p>
+       <p>Tipo: {categoriesConversion[category]}</p>
+       <p>Data:</p>
       </div>
     </div>
   );
--- src/pages/transactions/history/index.tsx
+++ src/pages/transactions/history/index.tsx
@@ -1,38 +1,45 @@
 import PageTitle from "@/components/atoms/PageTitle";
 import SearchField from "@/components/atoms/SearchField";
-import TransactionComponent from "@/components/atoms/TransactionComponent";
+import TransactionComponent, { categoriesConversion } from "@/components/atoms/TransactionComponent";
 import { getTransactions } from "@/service/api/transactions";
 import { useEffect, useState } from "react";
 
 interface transactionType {
-	id: string;
-	createdById: string;
-	updateById: string;
-	price: number;
-	description: string;
-	category: string;
+  id: string;
+  createdById: string;
+  updateById: string;
+  price: number;
+  description: string;
+  category: string;
+  createdAt: string;
 }
 
 export default function TransactionsHistory() {
+  const [transactions, setTransactions] = useState<transactionType[]>([]);
 
-	 const [transactions, setTransactions] = useState<transactionType[]>([]);
-
-	 useEffect(() => {
-		 getTransactions().then((data) => {
-			 setTransactions(data.content);
-		 });
-	 }, []);
+  useEffect(() => {
+    getTransactions().then((data) => {
+      setTransactions(data.content);
+      console.log(data.content);
+    });
+  }, []);
 
   return (
     <div>
       <PageTitle>Histórico de transações</PageTitle>
       <SearchField placeholder="Buscar transações" />
-			<div className="flex flex-col gap-5 my-3">
-				{transactions && transactions.map((transaction) => (
-					<TransactionComponent key={transaction.id} description={transaction.description} value={transaction.price} />
-				))}
-			</div>
-
+      <div className="flex flex-col gap-5 my-3">
+        {transactions &&
+          transactions.map((transaction) => (
+            <TransactionComponent
+              key={transaction.id}
+              description={transaction.description}
+              value={transaction.price}
+							category={transaction.category as keyof typeof categoriesConversion}
+              createdAt={transaction.createdAt}
+            />
+          ))}
+      </div>
     </div>
   );
 }
