f77a440eed3b93fe97f921346591219c3e3b9c01
WillianM19 <willianm1928@gmail.com>
2025-03-03 14:20:45 -0300

    feat(home): melhora titulos na responsividade desktop

--- src/components/molecules/MenuList/index.tsx
+++ src/components/molecules/MenuList/index.tsx
@@ -8,7 +8,7 @@ interface MenuListProps {
 export default function MenuList({ title, list }: MenuListProps) {
   return (
     <div>
-      <p className="text-[20px] mb-3">{title}</p>
+      <p className="text-[20px] mb-3 md:mb-6 md:mt-3 md:text-[24px] md:border-b md:pb-2 md:border-red-500/35">{title}</p>
       <div className="flex gap-4">
         {list.map((item, index) => (
           <MenuItem key={index} {...item} />
