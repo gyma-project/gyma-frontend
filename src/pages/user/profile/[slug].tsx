7f6f088bdf9a999367d4b1e5b0d3cdd8c75a5941
WillianM19 <willianm1928@gmail.com>
2025-03-05 12:15:26 -0300

    feat(profile): base da página

---
+++ src/pages/user/profile/[slug].tsx
@@ -0,0 +1,76 @@
+// pages/[slug].tsx
+
+import Button from '@/components/atoms/Button';
+import PageTitle from '@/components/atoms/PageTitle';
+import { getUUIDbyUsername } from '@/service/api/keycloak';
+import { getProfileByUuid } from '@/service/api/profiles';
+import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+type ProfileProps = {
+  slug: string;
+};
+
+export default function Profile({ slug }: ProfileProps) {
+  const [profile, setProfile] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <p className="text-red-500 font-semibold text-lg">Carregando perfil...</p>
+      </div>
+    );
+  }
+
+  if (!profile) {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-red-50">
+        <p className="text-red-600 font-bold text-xl">Perfil não encontrado.</p>
+      </div>
+    );
+  }
+
+  const router = useRouter()
+
+  return (
+    <div>
+      <PageTitle>Perfil do usuário {profile.firstName}</PageTitle>
+      <div className="w-full mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+        <div className="bg-red-500 text-white px-6 py-4">
+          <p className="text-sm">Informações do usuário cadastradas no sistema GYMA</p>
+        </div>
+        <div className="px-6 py-8 space-y-4">
+          <div>
+            <p className="text-gray-600 font-medium">Nome de usuário:</p>
+            <p className="text-red-700 text-lg font-semibold">{profile.username}</p>
+          </div>
+          <div>
+            <p className="text-gray-600 font-medium">Nome:</p>
+            <p className="text-red-700 text-lg font-semibold">{profile.firstName}</p>
+          </div>
+          <div>
+            <p className="text-gray-600 font-medium">Sobrenome:</p>
+            <p className="text-red-700 text-lg font-semibold">{profile.lastName}</p>
+          </div>
+          <div className='pb-16'>
+            <p className="text-gray-600 font-medium">Email:</p>
+            <p className="text-red-700 text-lg font-semibold">{profile.email}</p>
+          </div>
+          <Button onClick={() => router.back()}>Voltar a página anterior</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { slug } = context.params as { slug: string };
+
+  return {
+    props: {
+      slug,
+    },
+  };
+};
--- src/service/api/profiles.ts
+++ src/service/api/profiles.ts
@@ -24,3 +24,4 @@ export const createProfile = async (profileData: ProfileData) => {
     return [];
   }
 };
+
