c92d0f41c6566db9da288695a47bd6aab41ff220
WillianM19 <willianm1928@gmail.com>
2025-03-06 17:37:33 -0300

    feat: base de cards

--- src/components/atoms/UserListCard/index.tsx
+++ src/components/atoms/UserListCard/index.tsx
@@ -1,11 +1,14 @@
-export default function UserListCard() {
-  return(
-    <div className="w-[400px] h-[175px] bg-red-50 rounded-xl shadow-md hover:bg-red-100 cursor-pointer">
-      <div>
+import Image from "next/image"
 
+export default function UserListCard({user}: any) {
+  return(
+    <div className="w-[350px] h-[175px] bg-red-50 rounded-xl shadow-md hover:bg-red-100 cursor-pointer flex items-center p-3">
+      <div className="border rounded-full border-red-500/50">
+        <img src={`http://localhost:9000/images/${user.keycloakId}.jpg`} alt={user.firstName} width={100} height={100} />
       </div>
-      <div>
-        
+      <div className="p-6">
+        <p className="text-[22px] text-ellipsis overflow-hidden whitespace-nowrap capitalize text-red-500 w-[200px]">{user.firstName} {user.lastName}</p>
+        <p className="text-red-500 text-ellipsis overflow-hidden whitespace-nowrap w-[200px]"><strong>Email: </strong>{user.email}</p>
       </div>
     </div>
   )
--- src/pages/user/list-students/index.tsx
+++ src/pages/user/list-students/index.tsx
@@ -1,12 +1,28 @@
 import PageTitle from "@/components/atoms/PageTitle";
 import UserListCard from "@/components/atoms/UserListCard";
+import { getProfiles, Role } from "@/service/api/profiles";
+import { useEffect, useState } from "react";
 
 
 export default function ListStudents() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    getProfiles().then((res) => {
+    // getProfiles(Role.STUDENT).then((res) => {
+      setUsers(res.content);
+    });
+  }, [])
+
   return (
     <div>
       <PageTitle>Listagem de Alunos</PageTitle>
-      <UserListCard />
+      <div className="flex flex-wrap gap-4">
+        {users.map((user: any) => (
+          <UserListCard user={user} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
